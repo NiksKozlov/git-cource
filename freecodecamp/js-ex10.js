9a83464f1b1911c74683aba898af6ce5cfa5f2e1
NiksKozlov <nikskozlov@gmail.com>
2022-07-31 03:15:02 +0300

    new add to file ex js10

--- freecodecamp/js-ex10.js
+++ freecodecamp/js-ex10.js
@@ -5,3 +5,12 @@ function greet(name){
       return "Hello, " + name + "!";
     }
   }
+
+  function areYouPlayingBanjo(name) {
+    let firstLetter = name.charAt(0)
+    if (firstLetter == 'R' || firstLetter == 'r') {
+      return name + ' plays banjo'
+    } else {
+      return name + ' does not play banjo'
+    }
+  }
